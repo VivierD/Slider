c008ce99a835e806b04b42181220fa98b241102a
VivierD <96871948+VivierD@users.noreply.github.com>
2023-02-16 14:21:14 +0100

    Add files via upload

---
+++ src/Components/BtnSlider.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import leftArrow from './icons/left-arrow.svg'
+import rightArrow from './icons/right-arrow.svg'
+
+function BtnSlider({direction, moveSlide}) {
+  return (
+    <button 
+    onClick={moveSlide}
+    className={direction === "next" ? "btn-slide next" : "btn-slide prev"}>
+        <img src={direction === "next" ? rightArrow : leftArrow} alt ="icone flèches" />
+    </button>
+  )
+}
+
+export default BtnSlider
---
+++ src/Components/Slider.js
@@ -0,0 +1,78 @@
+import {useState} from 'react'
+import './Slider.css'
+import dataSlider from './dataSlider'
+import BtnSlider from './BtnSlider'
+
+export default function Slider() {
+
+const [slide, setSlide] = useState({
+    index : 1,
+    inProgress : false
+})
+
+const nextSlide = () => {
+    if(slide.index !== dataSlider.length && !slide.inProgress){
+
+        setSlide({index : slide.index + 1, inProgress : true})
+
+        setTimeout(()=>{
+            setSlide({index: slide.index +1, inProgress : false})
+        }, 400)
+    }
+    else if(slide.index === dataSlider.length && !slide.inProgress){
+        setSlide({index : 1 , inProgress : true})
+
+        setTimeout(()=>{
+            setSlide({index: 1 , inProgress : false})
+        }, 400)
+    }
+
+}
+
+const prevSlide = () => {
+    if(slide.index !== 1 && !slide.inProgress){
+        setSlide({index : slide.index - 1, inProgress : true})
+
+        setTimeout(()=>{
+            setSlide({index: slide.index - 1 , inProgress : false})
+        }, 400)
+    }
+    else if(slide.index === 1 && !slide.inProgress){
+        setSlide({index : dataSlider.length , inProgress : true})
+
+        setTimeout(()=>{
+            setSlide({index: dataSlider.length , inProgress : false})
+        }, 400)
+    }
+}
+
+    const moveDot = index => {
+        setSlide({index : index, inProgress : false})
+    }
+
+  return (
+    <div className="container-slider">
+        {dataSlider.map((obj, index) => {
+            return(
+                <div 
+                key={obj.id}
+                className={slide.index === index + 1 ? 
+                "slide active-anim" : "slide"}
+                >
+                    <img src={process.env.PUBLIC_URL + `/Imgs/img${index + 1}.jpg`} alt ="" />
+                </div>
+            )
+        })}
+        <BtnSlider moveSlide = {nextSlide} direction = {"next"} />
+        <BtnSlider moveSlide = {prevSlide} direction = {"prev"} />
+        <div className='container-dots'>
+            {Array.from({length:dataSlider.length}).map((item, index) => {
+                return <button 
+                className={slide.index === index + 1 ? "dot active" : "dot"}
+                onClick={()=>moveDot(index + 1)}
+                ></button>
+            })}
+        </div>
+    </div>
+  )
+}
